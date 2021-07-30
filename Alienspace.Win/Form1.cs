4b310aded7112f467af8559161267c0a91353e44
josue240 <71288779+josue240@users.noreply.github.com>
2021-07-29 18:01:27 -0600

    Add files via upload

---
+++ Alienspace.Win/Form1.cs
@@ -0,0 +1,30 @@
+﻿using AlienspaceBL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Alienspace.Win
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            var peliculasBL = new PeliculasBL();
+            var listadePeliculas = peliculasBL.ObtenerPeliculas();
+            listadePeliculasBindingSource.DataSource = listadePeliculas;
+        }
+    
+        // Not touch
+        private void listadePeliculasBindingSource_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Alienspace.Win/Form2.cs
@@ -0,0 +1,37 @@
+﻿using AlienspaceBL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Alienspace.Win
+{
+    public partial class Form2 : Form
+    {
+        ReportesPeliculasBL _ReportesPeliculasBL;
+
+        public Form2()
+        {
+            InitializeComponent();
+            _ReportesPeliculasBL = new ReportesPeliculasBL();
+            RefreshDatas();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RefreshDatas();
+        }
+
+        private void RefreshDatas()
+        {
+            var listaReportesPeliculas = _ReportesPeliculasBL.ObtenerReportesPeliculas();
+            listaReportesPeliculasBindingSource.DataSource = listaReportesPeliculas;
+            listaReportesPeliculasBindingSource.ResetBindings(false);
+        }
+    }
+}
